0ae84315155e1172056a2c29308597255981fc86
Julio Soto <jsberlanga@gmail.com>
2019-08-01 07:28:10 +0200

    fix seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -36,10 +36,6 @@ function SEO({ description, lang, meta, keywords, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
-          name=`theme-color`,
-          content="#eac100"
-        },
-        {
           name: `description`,
           content: metaDescription,
         },
